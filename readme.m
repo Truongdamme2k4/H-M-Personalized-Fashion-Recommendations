def4293af99cbbc2033af1b1b9fa5794a27866db
TuanAnh <anhnt204.dev@gmail.com>
2026-03-08 19:49:48 +0700

    add spark to docker

--- readme.m
+++ readme.m
@@ -1,22 +1,13 @@
-docker swarm init --advertise-addr <IP_MAY_1>
-
-docker swarm init --advertise-addr <IP_MAY_1>
-
 docker node ls
 
-#Overlay network
-docker network create --driver overlay --attachable bigdata-net
-
-docker stack deploy -c hdfs.yml hdfs
-
-
-deploy:
-  placement:
-    constraints:
-      - node.role == worker
-
-
-docker exec -it zerotier-one zerotier-cli join <NETWORK_ID_CỦA_BẠN>
+docker exec -it zerotier-one zerotier-cli join 154a350c86b1c5c7
 docker swarm init --advertise-addr 10.229.91.65
 docker network create --driver overlay --attachable bigdata_network
-docker swarm join --token SWMTKN-1-365xz1fpdo2tbjd24o4pew6tzgdik2bj5stlxcs7j1flo27948-4oqb15s70wd6x8dq6voykxq8x 10.229.91.65:2377
+docker swarm join --token SWMTKN-1-04wa2eqvgerut1laabrrkmr4kyl0lxkm5seodx9mu4x60rny19-9dlq5mk748codvep494ty2sw3 10.229.91.65:2377
+docker stack deploy -c docker-stack.yml hadoop_cluster
+docker stack rm hadoop_cluster
+
+docker pull bde2020/hadoop-namenode:2.0.0-hadoop3.2.1-java8
+docker pull bde2020/hadoop-datanode:2.0.0-hadoop3.2.1-java8
+docker pull bde2020/hadoop-resourcemanager:2.0.0-hadoop3.2.1-java8
+docker pull bde2020/hadoop-nodemanager:2.0.0-hadoop3.2.1-java8
